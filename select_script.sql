de75205a2f63d0da6b4c447f68a6494bee401bb4
hsmgowtham <hsmgowtham@gmail.com>
2023-11-27 00:10:22 -0500

    add sample query

--- select_script.sql
+++ select_script.sql
@@ -4,9 +4,11 @@ select * from friendship;
 select * from post_comment;
 select * from post_like;
 
+-- Display profile and their friends
 Select up1.given_name as name, up2.given_name as friend 
 from user_profile up1
 Join friendship
-on friendship.id = up1.id
+on friendship.profile_accept = up1.id
 Join user_profile up2
-on friendship;  
+on friendship.profile_request = up2.id
+order by up1.given_name;  
