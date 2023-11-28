33424e96d0918d56397ae5ae7831116d1b88b23f
hsmgowtham <hsmgowtham@gmail.com>
2023-11-28 11:34:22 -0500

    add queries

--- queries.sql
+++ queries.sql
@@ -4,11 +4,11 @@ select * from friendship;
 select * from post_comment;
 select * from post_like;
 
--- Display profile and their friends
-Select up1.given_name as name, up2.given_name as friend 
+-- Display user_profile and their friends
+Select up1.given_name as user_profile, up2.given_name as friend 
 from user_profile up1
 Join friendship
-on friendship.profile_accept = up1.id
+on friendship.profile_accept = up1.id and friendship.status='accepted'
 Join user_profile up2
 on friendship.profile_request = up2.id
-order by up1.given_name;  
+order by up1.given_name;    
